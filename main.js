541be0f50465ac883e8b589a7255cd3243cceee4
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-16 16:21:32 +0900

    条件分岐

--- main.js
+++ main.js
@@ -5,8 +5,10 @@
 console.log(50 + '20');
 
 //promptの場合は入力された数は文字列になる
-const n = prompt('Any number?');
+const score = Number(prompt('Any number?'));
 
-//そのため、足し算する場合はキャストする必要がある
-console.log(Number(n) + 10);
-
+if (score >= 90) {
+  console.log('A!')
+} else {
+  console.log('not A!');
+}
