8e8bbec071b86b2ca6d3ef50cac2581a78b96f3f
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-17 20:12:21 +0900

    早期リターン

--- main.js
+++ main.js
@@ -3,15 +3,19 @@
 {
   //引数のデフォルト値
   function calculateTotal(price, amount, rate = 1.1) {
-    return price * amount * rate;
+    // if (amount >= 100) {
+    //   return price * amount * 1;
+    // } else {
+    //   return price * amount * rate;
+    // }
+
+    //早期リターン
+    if (amount >= 100) {
+      return price * amount * 1;
+    } 
+      return price * amount * rate;
   }
 
-
-  console.log(calculateTotal(100, 10));
-  console.log(calculateTotal(150, 10));
-  console.log(calculateTotal(200, 10));
-  console.log(calculateTotal(200, 10, 1.08));
-
-
-
+  console.log(calculateTotal(100, 100));
+  console.log(calculateTotal(1000, 10));
 }
