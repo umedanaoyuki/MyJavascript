30afb60da0d0194c355f889059afcd10badfe3c4
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-17 19:19:05 +0900

    for文の中のfor文

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 "use strict";
 
-for (let i = 0; i < 10; i++) {
-  console.log(`${i} Hello`);
+let price;
+const rate = 1.1;
+
+for (price = 150; price <= 160; price++) {
+  console.log(`Price: ${price}`);
+
+  for (let amount = 120; amount < 140; amount += 10) {
+    console.log(price * amount * rate);
+  }
 }
+
+
+
+
+// price = 150;
+// console.log(price * 120 * rate);
+// console.log(price * 130 * rate);
+// console.log(price * 140 * rate);
+
+// price++;
+// console.log(price * 120 * rate);
+// console.log(price * 130 * rate);
+// console.log(price * 140 * rate);
