a212557c9753001178581e56e4198c3a1733bcc7
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-17 19:03:31 +0900

    pretier

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 //JavaScriptの場合は
 //数字が文字列に自動的に変更される
-console.log(50 + '20');
+console.log(50 + "20");
 
 //promptの場合は入力された数は文字列になる
-const score = Number(prompt('Any number?'));
+const score = Number(prompt("Any number?"));
 
 if (score >= 90) {
-  console.log('A!')
+  console.log("A!");
 } else {
-  console.log('not A!');
+  console.log("not A!");
 }
