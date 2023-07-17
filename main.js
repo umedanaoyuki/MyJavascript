0c8eab630d316e9745a193baced5fd5324864957
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-17 19:39:33 +0900

    条件演算子

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
 "use strict";
 
-let amount = 100;
+const score = Number(prompt('Score?'));
 
-for (let year = 1; year <= 30; year++) {
-  amount = amount * 1.05;
+// 条件演算子
+const result = score > 80 ? 'A' : 'B';
+console.log(result);
 
-  if (amount < 200) {
-    continue;
-    //これ以降の処理は実施されないのでconsolo.logの中身が表示されず
-    //今回のコードの場合は、200より少ない場合のconsole.logは表示されず
-    //300より上のamountも表示されない
-  }
-  if (amount > 300) {
-    break;
-  }
-  console.log(`Year ${year}: ${amount}`);
-}
-
-
-  
