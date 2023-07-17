22e4f484148f8d28a80ff79e740d3dbb794221a6
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-17 19:12:13 +0900

    テンプレートリテラルとfor文

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
 "use strict";
 
-const color = prompt("Color?");
-
-switch (color) {
-  case "red":
-    console.log("Stop!");
-    break;
-
-  case "yello":
-    console.log("Slow down");
-    break;
-
-  case "blue":
-  case "green":
-    console.log("Go!");
-    break;
-
-  default:
-    console.log("wrong color");
-    break;
+for (let i = 0; i < 10; i++) {
+  console.log(`${i} Hello`);
 }
