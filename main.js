8196e09728d058f3035cae83760096c5f1f8f9f2
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-17 20:05:58 +0900

    関数の戻り値がvoidの場合→undefined

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 "use strict";
 
 {
-//   function sum(num1, num2) {
-//     return num1 + num2;
-//   }
+  function sum(num1, num2) {
+    return num1 + num2;
+  }
 
+  // console.log(sum(300,700));
 
-// console.log(sum(300,700));
+  // function sum(num1, num2) {
+  //   console.log(num1 + num2);
+  //   //リターンを書かない場合は undefinedになっている
+  //   // return undefined;
+  // }
 
+  //undefinedが表示されるはず
+  // console.log(sum(300,700));
 
-
-function sum(num1, num2) {
-  console.log(num1 + num2);
-}
-
-sum(300,700);
-
-
-
+  console.log(sum(300, 700) * 3);
 }
