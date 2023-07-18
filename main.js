dedbee0232d1d33ba6c679fb52cd9db79e8cd9a3
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-18 23:51:59 +0900

    Join, Split

--- main.js
+++ main.js
@@ -1,33 +1,19 @@
 "use strict";
 
-//オブジェクトの宣言
 {
-  const scores = {
-    math: 80,
-    english: 90,
-  };
 
- let sum = 0;
+  const names = ['Taro', 'Jiro', 'Saburo'];
 
- //配列に要素を追加
- scores.physics = 70;
+  console.log(names.join('|'));
 
-  //オブジェクトを配列へ変更
-  const enttries = Object.entries(scores);
-  console.log(enttries);
-
-  enttries.forEach((prop) => {
-    sum += prop[1];
-    //console.log(prop);
-    //要素とvalueを別々で表示させる
-    console.log(`${prop[0]}: ${prop[1]}`);
-  }) 
+  //カンマでつなげる
+console.log(names.join(''));
   
-  //合計
-  console.log(`SUM: ${sum}`);
-  //平均
-  console.log(`Average: ${sum / enttries.length}`);
+//そのまま繋げる
+console.log(names.join());
 
+const array = 'Taro|Jiro|Saburo';
+console.log(array.split('|'));
 
 
 }
