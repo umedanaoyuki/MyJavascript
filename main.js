d9085ae7c1b51699f8b708e14723c2075a5e3ae3
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-18 23:41:17 +0900

    オブジェクトを配列へ変更

--- main.js
+++ main.js
@@ -2,18 +2,21 @@
 
 //オブジェクトの宣言
 {
-
   const scores = {
     math: 80,
-    english: 90
-  }
+    english: 90,
+  };
 
+  //オブジェクトを配列へ変更
+  const enttries = Object.entries(scores);
+  console.log(enttries);
 
-  scores.physics = 70;
-  console.log(scores);
+  enttries.forEach((prop) => {
+    //console.log(prop);
+    //要素とvalueを別々で表示させる
+    console.log(`${prop[0]}: ${prop[1]}`);
+  }) 
 
-  delete scores.english;
-  console.log(scores);
 
 
 }
