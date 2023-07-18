4e7b4fc22320fc6f716a2bfe08182f9a5f3b5146
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-18 23:04:19 +0900

    配列

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
 "use strict";
 
 {
-
-  const double = (num) => {
-    return num * 2;
-  };
-
-  // 仮引数にfuncという引数の関数を入力している（つまりnumをインプットすると
-  // 何かしらの処理を自動的にしてくれるcalc関数を定義してくれている）
-  const calc = (num, func) => {
-    return func(num);
-  };
-
-  //今回　上で定義した仮引数の関数に対して doubleを指定している
-  // double()にしない
-  // console.log(calc(20, double));
-
-  //わざわざアロー関数を書かずに、直接この中にアロー関数を書くことができる
-  console.log(calc(20, (num) => {
-    return num * 2;
-  }));
+  const socres = [
+    70,90,80,85
+  ]
 
 
+  console.log(socres);
 
 }
