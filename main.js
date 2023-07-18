ad8df772e7ec003637bc002a995fee04b87f4f99
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-18 23:46:26 +0900

    オブジェクトの中の操作（オブジェクト→配列への変換後）

--- main.js
+++ main.js
@@ -7,15 +7,26 @@
     english: 90,
   };
 
+ let sum = 0;
+
+ //配列に要素を追加
+ scores.physics = 70;
+
   //オブジェクトを配列へ変更
   const enttries = Object.entries(scores);
   console.log(enttries);
 
   enttries.forEach((prop) => {
+    sum += prop[1];
     //console.log(prop);
     //要素とvalueを別々で表示させる
     console.log(`${prop[0]}: ${prop[1]}`);
   }) 
+  
+  //合計
+  console.log(`SUM: ${sum}`);
+  //平均
+  console.log(`Average: ${sum / enttries.length}`);
 
 
 
