040ab97e1721d7533cf796a44f5403c1b1ab3a11
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-31 23:06:29 +0900

    join/Split

--- main.js
+++ main.js
@@ -2,20 +2,14 @@
 
 {
 
-  const scores = [70, 90, 80, 85];
+  // const names = ['Taro', 'Jiro', 'Saburo'];
 
-  scores.splice(2, 0, 77, 88);
-  // 70, 90, 77, 88, 80, 85
+  // // Taro|Jiro|Saburo
+  // console.log(names.join('|'));
+  // console.log(names.join());
+  // console.log(names.join(''));
 
-  //console.log(scores);
-  const deleted = scores.splice(3,1);
-  // 70, 90, 77, 80, 85
-  // deleted = [88] deleted が88が1つだけ入った配列
-
-  scores.splice(2,2,30);
-  // 70, 90, 30, 85
-
-  console.log(scores);
-  console.log(deleted);
+const names = 'Taro|Jiro|Saburo';
+console.log(names.split('|'));
 
 }
