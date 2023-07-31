b12e863e945eb2cad34852d27a1f193df57241f7
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-07-31 23:45:07 +0900

    スプレッド構文　レスト構文

--- main.js
+++ main.js
@@ -2,15 +2,14 @@
 
 {
 
-  const prices = [100, 150, 200];
+ const moreSocres = [77, 88];
+ //この書き方で配列をつなげる（スプレッド構文（右辺））
+ const socres = [70, 90, 80, 85, ...moreSocres];
+ //レスト構文（左辺）
+ const [first, ...others]= socres;
 
-  const array =
-  prices.filter((price) => {
-    return price >= 150;
-  });
-
-  console.log(array);
-
-
+ console.log(first);
+ console.log(others);
+ 
 
 }
