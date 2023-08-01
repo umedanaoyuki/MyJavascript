f614d23992d87ee956eae99cbde553008344a392
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-01 22:54:05 +0900

    toLowerCase, trim

--- main.js
+++ main.js
@@ -2,10 +2,14 @@
 
 {
 
-  const string = prompt('Any message?');
-  console.log(string);
-  console.log(string.length);
-  //最初の文字を表示
-  console.log(string[0]);
+  const string = prompt('Name?');
+
+  //空白を抜く、文字をすべて小文字に変換
+  if (string.toLowerCase().trim() === 'taro') {
+    console.log('correct!');
+  } else {
+    console.log('wrong!')
+  }
+
 
 }
