0d79c699ca5c4fc2893124f3309bb11847e8ef4e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-01 22:26:37 +0900

    toFixed

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
 
 {
 
-  const n = Number(prompt('Minutes?'));
-  console.log(`Hours: ${Math.floor(n /60)}`);
-  console.log(`Minites: ${n % 60}`);
+  const n = Number(prompt('Yen?'));
+  //2ケタ以降は四捨五入
+  const amount = (n/ 138.91).toFixed(2)
+  console.log(`USD: ${amount}`);
 
 
 }
