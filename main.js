8422e4841f5aad69b6f544cb68e4be2100363f69
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-01 22:33:35 +0900

    ％の演算子

--- main.js
+++ main.js
@@ -2,12 +2,15 @@
 
 {
 
-  const n = Number(prompt('Yen?'));
-  //2ケタ以降は四捨五入
-  //文字列
-  const amount = (n/ 138.91).toFixed(2)
-  //３ケタごとにコンマを入れる
-  console.log(`USD: ${Number(amount).toLocaleString()}`);
+  const n = Number(prompt('Count?'));
 
+  for (let i = 0; i < n; i++) {
+    if (i % 2 === 0) {
+      console.log(`--- item: ${i} ---`);
+    } else {
+      console.log(`=== item: ${i} ===`);
+    }
+  }
+  
 
 }
