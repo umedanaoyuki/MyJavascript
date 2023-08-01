b402fd4a0ed16d869c285cce56f802ed3f3621c1
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-01 22:37:41 +0900

    乱数

--- main.js
+++ main.js
@@ -2,14 +2,13 @@
 
 {
 
-  const n = Number(prompt('Count?'));
 
-  for (let i = 0; i < n; i++) {
-    if (i % 2 === 0) {
-      console.log(`--- item: ${i} ---`);
-    } else {
-      console.log(`=== item: ${i} ===`);
-    }
+  for (let i = 0; i < 10; i++) {
+    //乱数を生成(0以上1未満)
+    // const n = Math.random();
+    //2以下の乱数
+    const n = Math.random() * 3;
+    console.log(`${i}: ${Math.floor(n)}`);
   }
   
 
