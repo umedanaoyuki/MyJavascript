f92b82cd951cd31986bc287641ff808dadc540fa
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-01 22:28:24 +0900

    toLocalString

--- main.js
+++ main.js
@@ -4,8 +4,10 @@
 
   const n = Number(prompt('Yen?'));
   //2ケタ以降は四捨五入
+  //文字列
   const amount = (n/ 138.91).toFixed(2)
-  console.log(`USD: ${amount}`);
+  //３ケタごとにコンマを入れる
+  console.log(`USD: ${Number(amount).toLocaleString()}`);
 
 
 }
