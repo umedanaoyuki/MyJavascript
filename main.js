c21605f5cebfe426c3da7ae2a88f220861849297
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-02 00:06:02 +0900

    情報の取り出し方

--- main.js
+++ main.js
@@ -5,7 +5,13 @@
  const d = new Date();
  console.log(d);
  console.log(d.toLocaleString());
-
-  
+ console.log(d.getFullYear());
+ console.log(d.getMonth()); // 0, 1, 2, 3.....
+ console.log(d.getDate());
+ console.log(d.getHours());
+ console.log(d.getMinutes());
+ console.log(d.getSeconds());
+ console.log(d.getMilliseconds());
+ console.log(d.getDay()); // 0,1,2,3.....
 
 }
