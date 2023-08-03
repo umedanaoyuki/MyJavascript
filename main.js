d68201c82071eab8c4e7d507f283673fe4554d97
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-03 23:29:43 +0900

    setTimeoutの中でsetTimeout

--- main.js
+++ main.js
@@ -2,14 +2,18 @@
 
 {
 
- setTimeout(() => {
- 
-  // console.log('50% off');
- window.location.href = 'https://news.yahoo.co.jp/articles/dac9ffe495b325b420c7a515e078ade0f2374a70/comments';
+  setTimeout(() => {
+    console.log(new Date());
+    setTimeout(() => {
+      console.log(new Date());
+      setTimeout(() => {
+        console.log(new Date());
+      }, 1000)
+    }, 1000);
+  }, 1000);
 
-}, 3000);
 
-console.log('Page loaded');
+
 
 
 
