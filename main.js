8938d5f7c4622ad0d6624b710177d23e6b8239c0
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-03 23:58:43 +0900

    表示切り替え

--- main.js
+++ main.js
@@ -7,9 +7,12 @@
   const buttonElement = document.querySelector('button');
 
   buttonElement.addEventListener('click', () => {
-    
+    if (isDevMode === true) {
+      pElement.textContent = 'Dev Mode is Off';
+      isDevMode = false;
+    } else {
+      pElement.textContent = 'Dev Mode is On';
+      isDevMode = true;
+    }
   });
-
-
-
 }
