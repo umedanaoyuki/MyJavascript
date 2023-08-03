6f409acce356310f84870391ad3315a332c2e640
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-03 23:07:25 +0900

    setInterval

--- main.js
+++ main.js
@@ -2,15 +2,10 @@
 
 {
 
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const d = new Date(2001, 0, 1);
-
-  // January 01, Monday
-  const month = months[d.getMonth()];
-  const date = String(d.getDate()).padStart(2, '0');
-  const day = days[d.getDay()];
-  console.log(`${month} ${date}, ${day}`);
+  console.log(new Date());
+  setInterval(() => {
+    console.log(new Date());
+  }, 1000);
 
 
 }
