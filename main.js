17bd39f61da3dd4a00097635624bc239d36b3beb
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-03 23:46:17 +0900

    タイマー関係

--- main.js
+++ main.js
@@ -3,14 +3,18 @@
 {
  const pElement = document.querySelector('p');
  const buttonElement = document.querySelector('button');
-;
+ let timeoutId;
 
   //再帰関数
   function showclock() {
-    setTimeout( () => {
+    timeoutId = setTimeout( () => {
       pElement.textContent = new Date();
       showclock();
   }, 1000);}
+
+  buttonElement.addEventListener('click', () => {
+    clearTimeout(timeoutId);
+  });
 
   showclock();
 
