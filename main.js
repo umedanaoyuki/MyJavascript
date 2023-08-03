4717d97e098afa743db6755ee8472dfdad61cbdc
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-03 22:50:04 +0900

    日付のバックアップ

--- main.js
+++ main.js
@@ -4,10 +4,14 @@
 
   // 2000 4 11
  const d = new Date(2000, 3, 11);
-//  d.setDate(23);
-d.setDate(d.getDate() + 100);
- console.log(d.toLocaleDateString());
 
+//  const dBackup = new Date(d.getFullYear, d.getMonth, d.getDay);
+ const dBackup = new Date(d.getTime());
+d.setDate(23);
+
+// d.setDate(d.getDate() + 100);
+ console.log(d.toLocaleDateString());
+ console.log(dBackup.toLocaleDateString());
 
 
 }
