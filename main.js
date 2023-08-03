fede7e431d274847ad143e2c09e66c9fab736a19
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-03 22:45:41 +0900

    日時データの更新

--- main.js
+++ main.js
@@ -3,12 +3,9 @@
 {
 
   // 2000 4 11
- //const d = new Date(2000,3,11);
- 
- // 2000 2 末日？？？？？
- //3月0日（2月末日）を表示させることで、閏年を調べなくてもいい
- const d = new Date(2000, 2, 0);
-
+ const d = new Date(2000, 3, 11);
+//  d.setDate(23);
+d.setDate(d.getDate() + 100);
  console.log(d.toLocaleDateString());
 
 
