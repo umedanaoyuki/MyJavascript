dbcdab03471425af5862b2f48cc262c5a0d7028c
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-03 23:55:20 +0900

    true/falseの要素切り替え

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 "use strict";
 
 {
- const pElement = document.querySelector('p');
- const buttonElement = document.querySelector('button');
- let timeoutId;
 
-  //再帰関数
-  function showclock() {
-    timeoutId = setTimeout( () => {
-      pElement.textContent = new Date();
-      showclock();
-  }, 1000);}
+  let isDevMode = true;
+  const pElement  = document.querySelector('p');
+  const buttonElement = document.querySelector('button');
 
   buttonElement.addEventListener('click', () => {
-    clearTimeout(timeoutId);
+    
   });
 
-  showclock();
+
 
 }
